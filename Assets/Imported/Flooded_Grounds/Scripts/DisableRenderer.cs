3cb6a54455ecdd1ef57cb91211211dc56f387ef1
Jyotirmoy <jyotirmoy@elearningdesigncenter.com>
2024-12-02 17:17:31 +0530

    Pickup prefab setup and material

---
+++ Assets/Imported/Flooded_Grounds/Scripts/DisableRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableRenderer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().enabled = false;
+		
+	}
+	
+	
+}
